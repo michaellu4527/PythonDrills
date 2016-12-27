9ab6ddd32e99fda33452f0b5cb22dda9b775808f
Michael Lu <mklu2005@yahoo.com>
2016-12-27 13:42:57 -0800

    Finished basic structure of code

--- step62.py
+++ step62.py
@@ -5,38 +5,82 @@ import pytz
 #
 # print (current_utc)
 
+# Function obtains the current time in Portland and returns whether the store branch is open or not
+
 def getPDXTime():
     ptz = pytz.timezone('US/Pacific')
     current_utc = datetime.now(pytz.timezone('UTC'))
     pacific_time = current_utc.astimezone(ptz)
     pdx_time = pacific_time.strftime('%H:%M:%S')
-    print ("The current time in Portland is: " + pdx_time)
+    print ("\nThe current time in Portland is: " + pdx_time)
 
+    if pacific_time.hour >= 9 & pacific_time.hour < 21:
+        print ("\nThe current branch in Portland HQ is open! We hope to see you.")
+    else:
+        print ("\nWe're sorry, you just missed us. We're closed for the day. We're open from 9:00am to 9:00pm daily ")
+
+# Function obtains the current time in NYC and returns whether the store branch is open or not
 
 def getNewYorkTime():
     ptz = pytz.timezone('US/Eastern')
     current_utc = datetime.now(pytz.timezone('UTC'))
     eastern_time = current_utc.astimezone(ptz)
     nyc_time = eastern_time.strftime('%H:%M:%S')
-    print ("The current time in New York City is: " + nyc_time)
+    print ("\nThe current time in New York City is: " + nyc_time)
 
-    if eastern_time.hour >= 9 & eastern_time.hour <= 21:
+    if eastern_time.hour >= 9 & eastern_time.hour < 21:
         print ("\nThe current branch in NYC is open! We hope to see you.")
     else:
         print ("\nWe're sorry, you just missed us. We're closed for the day. We're open from 9:00am to 9:00pm daily ")
+
+# Function obtains the current time in London and returns whether the store branch is open or not
 
 def getLondonTime():
-    ptz = pytz.timezone('US/Eastern')
+    ptz = pytz.timezone('Europe/London')
     current_utc = datetime.now(pytz.timezone('UTC'))
-    eastern_time = current_utc.astimezone(ptz)
-    nyc_time = eastern_time.strftime('%H:%M:%S')
-    print ("The current time in New York City is: " + nyc_time)
+    gmt_time = current_utc.astimezone(ptz)
+    london_time = gmt_time.strftime('%H:%M:%S')
+    print ("\nThe current time in London is: " + london_time)
 
-    if eastern_time.hour >= 9 & eastern_time.hour <= 21:
-        print ("\nThe current branch in NYC is open! We hope to see you.")
+    if gmt_time.hour >= 9 & gmt_time.hour < 21:
+        print ("\nThe current branch in London is open! We hope to see you.")
     else:
         print ("\nWe're sorry, you just missed us. We're closed for the day. We're open from 9:00am to 9:00pm daily ")
 
+# Function that will obtain user input and run the corresponding function to get the user their requested
+# information
 
+def getBranchInfo():
+    flag = 1
+    while (flag == 1):
+        choice = raw_input("\nWe have a branch in 3 cities (Portland [HQ], New York City, and London)."
+                           "\nTo see if the branch is currently open, please enter (p, n, or l): ")
+        if choice == 'p':
+            getPDXTime()
+            anotherChoice()
+            if (anotherChoice() == 'y'):
+                flag = 1
+            elif (anotherChoice() == 'n'):
+                flag = 0
+        elif choice == 'n':
+            getNewYorkTime()
+            if (anotherChoice() == 'y'):
+                flag = 1
+            elif (anotherChoice() == 'n'):
+                flag = 0
+        elif choice == 'l':
+            getLondonTime()
+            if (anotherChoice() == 'y'):
+                flag = 1
+            elif (anotherChoice() == 'n'):
+                flag = 0
+        else:
+            print ("Invalid choice! Please try again...")
 
-getNewYorkTime()
+# Function that handles the possibility of the user wanting to view if another store is open or not
+
+def anotherChoice():
+    choice = raw_input("\nDo you want to see if another branch is open? [y/n] ")
+    return choice
+
+getBranchInfo()
